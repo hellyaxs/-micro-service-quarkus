28dc959972977e5bfd54930a293f6ea1209c4a8f
Elias <eliasvitor.dev@gmail.com>
2024-09-17 23:06:30 -0300

    pastar db/migration criada - bug corrigido

---
+++ election-management/src/main/java/infra/repositories/entities/CandidateEntity.java
@@ -0,0 +1,21 @@
+package infra.repositories.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class CandidateEntity {
+
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ election-management/src/test/java/infra/repositories/SQLCandidateRespositoryTest.java
@@ -0,0 +1,7 @@
+package infra.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQLCandidateRespositoryTest {
+
+}
